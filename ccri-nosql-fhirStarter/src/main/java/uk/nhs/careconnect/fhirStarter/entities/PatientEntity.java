f86a66b5098a6c88f1d80c2f1f76d7724e9d87fa
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2018-03-09 12:41:21 +0000

    Expanded the fhirStarter to more complete Patient record

--- ccri-nosql-fhirStarter/src/main/java/uk/nhs/careconnect/fhirStarter/entities/PatientEntity.java
+++ ccri-nosql-fhirStarter/src/main/java/uk/nhs/careconnect/fhirStarter/entities/PatientEntity.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.*;
 
 
-@Document(collection = "docPatient")
+@Document(collection = "idxPatient")
 public class PatientEntity  {
 
     @Id
